a2cf85685723b98bdb4d90635d1562bb3894dc6c
Alex <admin@aleksandrslarionovs.com>
2024-12-13 13:29:16 +0000

    6.2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const session = require("express-session");
 var ejs = require("ejs");
 var bodyParser = require("body-parser");
 var mysql = require("mysql2");
+var cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const local = false;
@@ -27,6 +28,7 @@ global.db = db;
 const app = express();
 const port = 8000;
 app.use(express.json({ limit: "5mb" }));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
 
